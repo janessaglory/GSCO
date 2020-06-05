e07a8f8020df5aca75e8ee804a940cdcf918e05d
janessaglory <66073300+janessaglory@users.noreply.github.com>
2020-06-05 18:36:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# GSCO
-car and wall
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,27 @@
+var car ,wall;
+var weight,speed;
+function setup() {
+  createCanvas(1600,400);
+  weight=random(400,1500);
+   speed=random(30,95);
+    wall= createSprite(1500, 200, 60, 400);
+     wall.shapeColor="grey";
+      car= createSprite(50, 100, 60, 30);
+       car.shapeColor="white";
+}
+
+function draw() {
+  background(0);  
+  car.velocityX=speed;
+   var deformation=(0.5*weight*speed*speed)/22500;
+    if(car.collide(wall)) {
+       if(deformation<100){
+          car.shapeColor=" green";
+         } else if(deformation<180&&deformation>100){
+            car.shapeColor="yellow";
+           } else if(deformation>180){ 
+             car.shapeColor="red";
+             }
+             } 
+  drawSprites();
+}
